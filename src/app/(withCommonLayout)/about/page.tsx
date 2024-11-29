8c0ca5a3417a360fede7033c5c8e59a5367d87cd
suaib022 <suaib.109206@gmail.com>
2024-11-29 16:32:46 +0600

    Initialize project with backend setup, routing, navbar, grouped routes, basic landing page, and data fetching services.

---
+++ src/app/(withCommonLayout)/about/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/src/components/primitives";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className={title()}>About This Page</h1>
+    </div>
+  );
+}
---
+++ src/app/(withCommonLayout)/found-items/page.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+
+const page = () => {
+  return (
+    // eslint-disable-next-line react/self-closing-comp
+    <div>
+      <h2>Found Items</h2>
+    </div>
+  );
+};
+
+export default page;
